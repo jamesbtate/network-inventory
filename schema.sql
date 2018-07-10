000a4e15d31377649e579a9b38f26f147b0233ea
James Tate II <jtate@odu.edu>
2018-07-10 14:39:04 -0400

    created schema for MVP

---
+++ schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE `device` (
+  `id` bigint unsigned not null primary key,
+  `name` varchar(100) not null,
+  `description` varchar(1000)
+) ENGINE=InnoDB;
+
+CREATE TABLE `note` (
+  `id` bigint unsigned not null primary key,
+  `device_id` bigint unsigned not null,
+  `noter` varchar(100) not null,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+  `value` varchar(5000),
+  FOREIGN KEY `device_fk` (`device_id`) REFERENCES `device` (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `attribute` (
+  `id` bigint unsigned not null primary key,
+  `name` varchar(100) not null,
+  `display_name` varchar(100),
+  `type` varchar(100),
+  `description` varchar(1000),
+  `free_form` bool not null,
+  `multi_value` bool not null
+) ENGINE=InnoDB;
+
+CREATE TABLE `class` (
+  `id` bigint unsigned not null primary key,
+  `name` varchar(100) not null,
+  `allow_other_attributes` bool not null
+) ENGINE=InnoDB;
+
+CREATE TABLE `class_attribute` (
+  `id` bigint unsigned not null primary key,
+  `class_id` bigint unsigned not null,
+  `attribute_id` bigint unsigned not null,
+  `allow_other_attributes` bool not null,
+  KEY (`class_id`, `attribute_id`),
+  FOREIGN KEY `class_fk` (`class_id`) REFERENCES `class` (`id`),
+  FOREIGN KEY `attribute_fk` (`attribute_id`) REFERENCES `attribute` (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `device_attribute` (
+  `id` bigint unsigned not null primary key,
+  `device_id` bigint unsigned not null,
+  `attribute_id` bigint unsigned not null,
+  `value` varchar(1000),
+  KEY (`device_id`, `attribute_id`),
+  FOREIGN KEY `device_fk` (`device_id`) REFERENCES `device` (`id`),
+  FOREIGN KEY `attribute_fk` (`attribute_id`) REFERENCES `attribute` (`id`)
+) ENGINE=InnoDB;

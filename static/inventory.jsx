8d3f983f4616d1700de7ad3297082ac289204bc3
James Tate II <jtate@odu.edu>
2018-07-12 17:17:13 -0400

    started using reusable React components.

--- static/inventory.jsx
+++ static/inventory.jsx
@@ -1,32 +1,47 @@
 // import React from 'react'
 // import ReactDOM from 'react-dom'
 
-class Form1 extends React.Component {
-  render() {
-    return (
-      <form action="/class/edit" method="post">
-        <label class="left" for="name">Name:</label>
-        <input class="right" size="24" type="text" name="name" />
-        <br />
-        <label class="left">Allow Other Attributes:</label>
-        <span class="right">
-          <label for="allow_other_attributes_yes">Yes</label>
-          <input type="radio" name="allow_other_attributes" id="allow_other_attributes_yes" value="yes" />
-          <label for="allow_other_attributes_no">No</label>
-          <input type="radio" name="allow_other_attributes" id="allow_other_attributes_no" value="no" />
-        </span>
 
-        <h3 class="left">Attributes</h3>
-
-        <input class="left ui-button ui-corner-all ui-widget" type="button" value="Add Attribute" />
-
-        <br />
-        <br />
-        <hr width="100%"/>
-        <input class="left ui-button ui-corner-all ui-widget" type="submit" />
-      </form>
-    )
-  }
+class YesNoInput extends React.Component {
+    render() {
+        return (
+            <span class="right">
+                <label for={this.props.name + "_yes"}>Yes</label>
+                <input type="radio" name={this.props.name} id={this.props.name + "_yes"} value="yes" />
+                <label for={this.props.name + "_no"}>No</label>
+                <input type="radio" name={this.props.name} id={this.props.name + "_no"} value="no" />
+            </span>
+        )
+    }
 }
 
-ReactDOM.render(<Form1 />, document.getElementById('root'));
+
+class ClassForm extends React.Component {
+    render() {
+        return (
+            <form action="/class/edit" method="post">
+                <label class="left" for="name">Name:</label>
+                <input class="right" size="24" type="text" name="name" />
+                <br />
+                <label class="left">Allow Other Attributes:</label>
+                <YesNoInput name="allow_other_attributes" />
+
+                <h3 class="left">Attributes</h3>
+
+                <input class="left ui-button ui-corner-all ui-widget" type="button" value="Add Attribute" />
+
+                <br />
+                <br />
+                <hr width="100%"/>
+                <input class="left ui-button ui-corner-all ui-widget" type="submit" />
+            </form>
+        )
+    }
+    componentDidMount(prevProps, prevState) {
+        $( "input[type='radio']" ).checkboxradio({
+            icon: false
+        });
+    }
+}
+
+ReactDOM.render(<ClassForm />, document.getElementById('root'));
